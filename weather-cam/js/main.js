aa71a08b417545efbc89b8dc55786863e6167ad8
EPdeHaas <96448767+EPdeHaas@users.noreply.github.com>
2024-07-21 09:39:41 +0000

    update

--- weather-cam/js/main.js
+++ weather-cam/js/main.js
@@ -1,22 +1,24 @@
 
 const ts_cams = [
-    { id: "boumaboulevard",         name: "Boumaboulevard",         large: true },
+    { id: "grote-markt",            name: "Grote Markt",            large: true },
+    { id: "grote-markt-2",          name: "Grote Markt 2" },
+    { id: "stationsweg",            name: "Stationsweg" },
+
+    { id: "boumaboulevard",         name: "Boumaboulevard" },
     { id: "europaweg",              name: "Europaweg" },
     { id: "friesestraatweg",        name: "Friesestraatweg" },
     { id: "gasunie",                name: "Gasunie" },
-    { id: "grote-markt",            name: "Grote Markt" },
-    { id: "grote-markt-2",          name: "Grote Markt 2" },
-    { id: "hoofdstation",           name: "Hoofdstation" },
-    { id: "hoofdstation-2",         name: "Hoofdstation 2" },
     { id: "julianaplein",           name: "Julianaplein" },
     { id: "julianaplein-2",         name: "Julianaplein 2" },
     { id: "laan-corpus-den-hoorn",  name: "Laan Corpus den Hoorn" },
     { id: "overwinningsplein",      name: "Overwinningsplein" },
     { id: "paterswoldseweg",        name: "Paterswoldseweg" },
-    { id: "stationsweg",            name: "Stationsweg" },
     { id: "vrijheidsplein",         name: "Vrijheidsplein" },
     { id: "winschoterdiep",         name: "Winschoterdiep" },
     { id: "zuiderplantsoen",        name: "Zuiderplantsoen" },
+
+    //{ id: "hoofdstation",           name: "Hoofdstation" },
+    //{ id: "hoofdstation-2",         name: "Hoofdstation 2" },
 ]
 
 function setup () {
@@ -57,7 +59,7 @@ function setup () {
 
 function createTile ( cam ) {
     let preview = document.createElement( 'img' );
-    preview.src = `./img/${ cam.id }.png`;
+    preview.src = `./${ cam.id }/screen.png`;
 
     let label = document.createElement( 'label' );
     preview.innerText = cam.name;
